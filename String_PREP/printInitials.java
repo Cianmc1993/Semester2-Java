f32745d352fdd3186507e6bdcd3b7bc3968b1372
Cian McLaughiin <cian.mclaughlin@hotmail.com>
2018-08-11 20:12:58 +0100

    Semester2-Java

--- String_PREP/printInitials.java
+++ String_PREP/printInitials.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class CianRevision
+public class printInitials
 {
    public static void main(String[] args) 
    { 
--- String_PREP/webAddress.java
+++ String_PREP/webAddress.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class toString
+public class webAddress
 {
    public static void main(String[] args)
    {
