c0483fa39de937dceb4cbed2deacbd75913c94b6
Cian McLaughiin <cian.mclaughlin@hotmail.com>
2018-08-11 16:15:29 +0100

    Semester2-Java

--- Objects/Oblong.java
+++ Objects/Oblong.java
@@ -7,60 +7,32 @@ import java.lang.Math;
 
 public class Oblong
 {
-    // the attributes are declared first
+   //Instance variables
    private double width;
    private double height;
 
-    // then the methods
-	
-	
-    // the next method allows us to get the width attribute
-	 /**
-		Gets the width of the oblong
-		@return  the width
-	*/
    public double getWidth()
    {
       return width;
    }
 
-    // the next method allows us to get the height attribute
-	 /**
-		Gets the height of the oblong
-		@return  the height
-	*/
    public double getHeight()
    {
       return height;
    }
 
-    // the next method allows us to set to the width attribute
-	 /**
-		Sets the width of the oblong.
-		@param widthIn the length
-	*/
    public void setWidth(double widthIn)
    {
       width = widthIn;
    } 
 
-    // the next method allows us to set to the height attribute
-	  /**
-		Sets the height of the oblong.
-		@param heightIn the height
-	*/
-
+ 
    public void setHeight(double heightIn)
    {
       height = heightIn;
    }
-
-    // this method returns the area of the oblong
-	  /**
-		Calculates the area of the oblong
-		@return  the area of the oblong
-	*/
-
+   
+   
    public double calculateArea()
    {
       return width * height;
