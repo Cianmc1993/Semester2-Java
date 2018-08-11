64ca44c6c3ce0742d000a26ae517d15ef56988b9
Cian McLaughiin <cian.mclaughlin@hotmail.com>
2018-08-11 21:06:10 +0100

    Semester2-Java

--- String_PREP/printInitials.java
+++ String_PREP/printInitials.java
@@ -11,10 +11,8 @@ public class printInitials
       System.out.print("Enter your full name: ");
       usersFullName=input.nextLine();
       
-      // the last indexOf(int ' ') method takes in a char (a ' '(space) in this example) and returns the index of the last occurence of the character past in
-      lastSpaceIndex=(usersFullName.lastIndexOf(' ')+1); // here i add 1 to it to give me the index after the second space which gives me the index of the 1 letter in the 3rd word in the string
-      
-      // now i just print that index
+      lastSpaceIndex=(usersFullName.lastIndexOf(' ')+1);      
+ 
       System.out.println(usersFullName.charAt(0)+ " "+
       usersFullName.charAt(lastSpaceIndex));
       
